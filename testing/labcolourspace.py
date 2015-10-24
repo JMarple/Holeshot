44a1beefe74e4eadf6a792826ab089227b96c37c
jmarple <jmarple@umass.edu>
2015-10-24 05:16:41 -0400

    Alot of vision code added.  Some good some bad

--- testing/labcolourspace.py
+++ testing/labcolourspace.py
@@ -11,10 +11,10 @@ high = np.array([255, 255, 255])
 def updateImage(x):
     pass
 
-#cv2.createTrackbar('Lh', 'testimage', 0, 255, updateImage)
+cv2.createTrackbar('Lh', 'testimage', 0, 255, updateImage)
 cv2.createTrackbar('Ah', 'testimage', 0, 255, updateImage)
 cv2.createTrackbar('Bh', 'testimage', 0, 255, updateImage)
-#cv2.createTrackbar('Ll', 'testimage', 0, 255, updateImage)
+cv2.createTrackbar('Ll', 'testimage', 0, 255, updateImage)
 cv2.createTrackbar('Al', 'testimage', 0, 255, updateImage)
 cv2.createTrackbar('Bl', 'testimage', 0, 255, updateImage)
 
@@ -25,25 +25,34 @@ labImage = cv2.cvtColor(im, cv2.COLOR_BGR2LAB);
 mask = cv2.inRange(labImage, low, high)
 output = cv2.bitwise_and(labImage, labImage, mask = mask)
 
-#cv2.setTrackbarPos('Lh', 'testimage', 255)
+cv2.setTrackbarPos('Lh', 'testimage', 255)
 cv2.setTrackbarPos('Ah', 'testimage', 255)
 cv2.setTrackbarPos('Bh', 'testimage', 255)
 
+cap = cv2.VideoCapture('testvideo4.avi')
+ret, frame = cap.read()
+labImage = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
+
 while(1):
-    low[0] = 0#cv2.getTrackbarPos('Ll', 'testimage')
+    low[0] = cv2.getTrackbarPos('Ll', 'testimage')
     low[1] = cv2.getTrackbarPos('Al', 'testimage')
     low[2] = cv2.getTrackbarPos('Bl', 'testimage')
-    high[0] = 255#cv2.getTrackbarPos('Lh', 'testimage')
+    high[0] = cv2.getTrackbarPos('Lh', 'testimage')
     high[1] = cv2.getTrackbarPos('Ah', 'testimage')
     high[2] = cv2.getTrackbarPos('Bh', 'testimage')
     
     mask = cv2.inRange(labImage, low, high)
-    output = cv2.bitwise_and(im, im, mask = mask)
-    weighted = cv2.addWeighted(output, 0.9, im, 0.1, 0) 
+    output = cv2.bitwise_and(frame, frame, mask = mask)
+    weighted = cv2.addWeighted(output, 0.9, frame, 0.1, 0) 
     cv2.imshow('testimage', weighted)
 
     k = cv2.waitKey(1) & 0xFF
     if k == 27:
         break
+
+    if k == ord('n'):
+        ret, frame = cap.read()
+        labImage = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB); 
     
+ 
 cv2.destroyAllWindows()
--- vision/findblobs.py
+++ vision/findblobs.py
@@ -3,40 +3,107 @@
 import cv2
 import numpy as np
 
+DEBUG = False
+
 # Takes in an LAB image and returns boxes showing where relevant objects are  
 def findBlobs(labImage):
    
-    orangeLow = np.array([0, 160, 140])
-    orangeHigh = np.array([255, 255, 255])
+    orangeLow = np.array([0, 145, 140])
+    orangeHigh = np.array([255, 200, 210])
 
     mask = cv2.inRange(labImage, orangeLow, orangeHigh)
     output = cv2.bitwise_and(labImage, labImage, mask = mask)
 
-    edged = cv2.Canny(output, 30, 200)   
-    
+    blurredOutput = cv2.blur(output, (25, 25))
+
+    ret, thresh1 = cv2.threshold(blurredOutput, 1, 255, cv2.THRESH_BINARY)
+
+    edged = cv2.Canny(thresh1, 30, 200)   
+ 
     (cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]    
- 
+    
+    objs = []
+     
     for c in cnts:
         peri = cv2.arcLength(c, True)
         approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-   
-        if peri < 40:
+        
+        if peri < 80:
             continue
-     
-        print len(approx)
-        print peri
-        print ""
+         
+        high = np.array([0,0])
+        low = np.array([10000, 10000])
 
+        # Find boundingbox
+        for obj in approx:
+            if obj[0][0] > high[0]:
+                high[0] = obj[0][0]
+            if obj[0][1] > high[1]:
+                high[1] = obj[0][1]
+            if obj[0][0] < low[0]:
+                low[0] = obj[0][0]
+            if obj[0][1] < low[1]:
+                low[1] = obj[0][1] 
+        
+        objs.append([0, low[0], low[1], high[0], high[1]])    
+        
         cv2.drawContours(labImage, [c], -1, (0, 255, 0), 3)
-     
+  
+    x1 = 1
+    x2 = 3
+    y1 = 2
+    y2 = 4 
+
+    height, width = labImage.shape[:2]
+    
+    CUTOFF_HEIGHT = 300 # Anything above this height (0 to 300) should not be considered a potentional object
+
+    cv2.rectangle(labImage, (0, CUTOFF_HEIGHT), (width, CUTOFF_HEIGHT), (255, 255, 0), 1)
+
+    for obj in objs:
+        if obj[0] != 0: continue
+ 
+        for checkObj in objs:
+
+            if obj is checkObj: continue
+            if checkObj[0] != 0: continue # Already found
+
+            # Check if intersecting, if not, check the next one
+            if obj[y2] < checkObj[y1]: continue
+            if checkObj[y2] < obj[y1]: continue
+            if obj[x2] < checkObj[x1]: continue
+            if checkObj[x2] < obj[x1]: continue
+           
+            obj[x1] = min(obj[x1], checkObj[x1])
+            obj[x2] = max(obj[x2], checkObj[x2])
+            obj[y1] = min(obj[y1], checkObj[y1])
+            obj[y2] = max(obj[y2], checkObj[y2])
+ 
+            checkObj[0] = 1 
+
+    for obj in objs:
+        if obj[0] != 0: continue
+        if obj[y2] < CUTOFF_HEIGHT: continue           
+
+        cv2.rectangle(labImage, (obj[x1], obj[y1]), (obj[x2], obj[y2]), (255,0,0), 1)
+ 
     return labImage
      
 if __name__ == "__main__":
-    img = cv2.imread("../testing/test.jpg")    
-    labImage = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
-        
-    output = findBlobs(labImage)
     
-    cv2.imshow('testimage', output)
-    cv2.waitKey(0)
+    cap = cv2.VideoCapture("../testing/testvideo3.avi")
+
+    while (cap.isOpened()):
+        ret, frame = cap.read()
+        
+        labImage = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
+
+        output = findBlobs(labImage)
+        
+        cv2.imshow('testimage', output)
+
+        if cv2.waitKey(0) & 0xFF == ord('q'):
+            break
+            
+    cap.release() 
